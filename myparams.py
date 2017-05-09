116ecc57034062f7b934659d601b00f2516ca460
Jan Dreyling-Eschweiler <jan.dreyling-eschweiler@desy.de>
2017-05-09 18:21:34 +0200

    new plot method

--- myparams.py
+++ myparams.py
@@ -16,8 +16,15 @@ nameRootHistos = np.array([
 
 ##################################################
 # Physical constants
+x0alu = 88.97 	# mm
+x0sil = 93.65 	# mm
+x0kap = 285.6 	# mm
+x0air = 3.042e5 # mm
 
-x0alu = 88.97 # mm
+# Telescope material budget, 20mm configuration
+thickness_mimosa = 6 * 50e-3 		# mm
+thickness_kapton = 12 * 25e-3 	# mm
+thickness_air    = 4 * 20 + 15 	# mm
 
 # Function
 def highland(momentum, thickness, x0):
@@ -27,3 +34,13 @@ def highland(momentum, thickness, x0):
     if epsilon > 100.:
       print "Warning! epsilon > 100 for thickness", thickness
     return 13.6/momentum*np.sqrt(epsilon)*(1.+0.038*np.log(epsilon))
+
+# Function
+def highland_multi_scatterer(momentum, thickness_dut, x0_dut):
+    epsilon_dut = thickness_dut/x0_dut
+    epsilon_total = epsilon_dut + thickness_mimosa/x0sil + thickness_kapton/x0kap + thickness_air/x0air
+    if epsilon_dut < 1e-3: 
+      print "Warning! epsilon < 0.001 for thickness", thickness_dut
+    if epsilon_dut > 100.:
+      print "Warning! epsilon > 100 for thickness", thickness_dut
+    return 13.6/momentum*np.sqrt(epsilon_dut)*(1.+0.038*np.log(epsilon_total))
